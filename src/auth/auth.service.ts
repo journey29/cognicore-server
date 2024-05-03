a0ed794a9b332683c267717b6b8312034523017a
Andrii Smaluniuk <smaluniuk.development@gmail.com>
2024-05-03 22:54:54 +0300

    chore:change cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -78,6 +78,7 @@ export class AuthService {
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
       expires: expiresIn,
       sameSite: 'none',
+      httpOnly: true,
       secure: true
     })
   }
@@ -86,6 +87,7 @@ export class AuthService {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
       secure: true,
       sameSite: 'none',
+      httpOnly: true,
       expires: new Date(0)
     })
   }

e1ba48800dad79f45b686eee6d3af5fab665ec22
Andrii Smaluniuk <smaluniuk.development@gmail.com>
2024-05-03 23:05:42 +0300

    change sameSite to lax cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -77,7 +77,7 @@ export class AuthService {
 
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
       expires: expiresIn,
-      sameSite: 'none',
+      sameSite: 'lax',
       domain: 'localhost',
       httpOnly: true,
       secure: true
@@ -87,7 +87,7 @@ export class AuthService {
   removeRefreshToken(res: Response) {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
       secure: true,
-      sameSite: 'none',
+      sameSite: 'lax',
       httpOnly: true,
       domain: 'localhost',
       expires: new Date(0)

876de4050ec56365351db6de68e8d2848c76e624
Andrii Smaluniuk <smaluniuk.development@gmail.com>
2024-05-03 22:36:53 +0300

    chore:delete httponly cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -76,7 +76,6 @@ export class AuthService {
     expiresIn.setDate(expiresIn.getDate() + this.REFRESH_TOKEN_EXPIRES_DATE)
 
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
-      httpOnly: true,
       expires: expiresIn,
       secure: true
     })
@@ -84,7 +83,6 @@ export class AuthService {
 
   removeRefreshToken(res: Response) {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
-      httpOnly: true,
       secure: true,
       expires: new Date(0)
     })

5e8150e131ad5a7e5a5afbd457e5cc0032d2d2cd
Andrii Smaluniuk <smaluniuk.development@gmail.com>
2024-05-04 14:51:43 +0300

    chore:change domain cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -78,7 +78,7 @@ export class AuthService {
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
       expires: expiresIn,
       sameSite: 'lax',
-      domain: process.env.CLIENT_URL,
+      domain: 'https://cognicore-client.vercel.app',
       httpOnly: true,
       secure: true
     })
@@ -88,7 +88,7 @@ export class AuthService {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
       secure: true,
       sameSite: 'lax',
-      domain: process.env.CLIENT_URL,
+      domain: 'https://cognicore-client.vercel.app',
       httpOnly: true,
       expires: new Date(0)
     })
